a8fb7c290996354114efd080cb27ed72de93f22a
shakharho <berdaddario@gmail.com>
2023-02-28 12:31:56 -0300

    docker image + d-c.yaml & product model

---
+++ models/Product.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema, model, Model} from 'mongoose';
+import { IProduct } from '../interfaces';
+
+
+const ProductSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    images: [
+        { type: String }
+    ],
+    inStock: { type: Number, required: true, default:0 },
+    price: { type: Number, required: true, default:0 },
+    sizes: [{ 
+        type: String, 
+        enum:{
+            values: ['XS', 'S', 'M', 'L', 'XL', 'XXL','XXXL'],
+            message: '{VALUE} is not supported'
+        }
+    }],
+    slug: { type: String, required: true, unique: true },
+    tags: [{ type: String }],
+    type:[{ 
+        type: String, 
+        enum:{
+            values: ['shirts', 'pants', 'hoodies', 'hats'],
+            message: '{VALUE} is not supported'
+        }
+    }],
+    gender: [{ 
+        type: String, 
+        enum:{
+            values: ['men', 'women', 'kid', 'unisex'],
+            message: '{VALUE} is not supported'
+        }
+    }],
+  },{
+    timestamps: true
+});
+
+const Product: Model<IProduct> = mongoose.models.Product || model<IProduct>('Product', ProductSchema);
+
+export default Product;
